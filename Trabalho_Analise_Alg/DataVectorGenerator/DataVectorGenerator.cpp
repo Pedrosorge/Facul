2fd88b59f4eb4ca7ce4d205c26bd2d801fd9ddb2
Pedro Sorge <pedrosorge2005@gmail.com>
2026-05-07 22:40:35 -0300

    Improving vector generator algorithms

--- Trabalho_Analise_Alg/DataVectorGenerator/DataVectorGenerator.cpp
+++ Trabalho_Analise_Alg/DataVectorGenerator/DataVectorGenerator.cpp
@@ -2,7 +2,6 @@
 #include <algorithm>
 #include <numeric>
 
-
 DataVectorGenerator::DataVectorGenerator() : generator(rd()) {};
 
 std::vector<int> DataVectorGenerator::genVectorRandomlyUniformed(unsigned int size){
@@ -17,20 +16,32 @@ std::vector<int> DataVectorGenerator::genVectorRandomlyUniformed(unsigned int si
 
 std::vector<int> DataVectorGenerator::genVectorAlreadyAscendentSorted(unsigned int size){
 
-    unsigned int curr = 1;
+    std::uniform_int_distribution<> dis(0,15);
+    unsigned int curr = dis(generator);
     std::vector<int> vet(size);
+    vet[0] = curr;
+    dis.param(std::uniform_int_distribution<int>::param_type(0,size));
 
-    std::iota(vet.begin(),vet.end(), curr);
+    std::generate(vet.begin()+1,vet.end(), [&]() {
+        curr+=dis(generator);
+        return curr;
+    });
 
     return vet;
 };
 
-std::vector<int> DataVectorGenerator::genVectorAlreadyDecrescentSorted(unsigned int size){
+std::vector<int> DataVectorGenerator::genVectorAlreadyDecrescentSorted(unsigned int size){    
     
+    std::uniform_int_distribution<> dis(2*size,5*size);
+    unsigned int curr = dis(generator);
     std::vector<int> vet(size);
-    unsigned int curr = size;
-    
-    std::generate(vet.begin(),vet.end(), [&]() { return size--; });
+    dis.param(std::uniform_int_distribution<int>::param_type(0,curr/size-1));
+
+    std::generate(vet.begin(),vet.end(), [&]() {
+        curr-=dis(generator);
+        return curr;
+    });
+
     return vet;
 
 };
@@ -38,7 +49,15 @@ std::vector<int> DataVectorGenerator::genVectorAlreadyDecrescentSorted(unsigned 
 std::vector<int> DataVectorGenerator::genVectorWithRepetitionOfElements(unsigned int size){
     
     std::vector<int> vet(size);
-    std::uniform_int_distribution<> dis(0,size/2);
+    std::uniform_int_distribution<> dis(0,log10(size)+1);
+    unsigned int deslocation = 1; 
+    unsigned int aux = dis(generator);
+
+    while(aux--){
+        deslocation*=10;
+    }
+
+    dis.param(std::uniform_int_distribution<int>::param_type(deslocation,deslocation+size/2));
 
     std::generate(vet.begin(),vet.end(), [&](){ return dis(generator); });
 
